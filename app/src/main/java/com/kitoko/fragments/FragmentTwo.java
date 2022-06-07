93ba5b1c649e2580f8384b34111fe930425062df
pbokolo <pbokolo1@gmail.com>
2022-06-07 21:24:16 +0300

    Fragments lecon

---
+++ app/src/main/java/com/kitoko/fragments/FragmentTwo.java
@@ -0,0 +1,11 @@
+package com.kitoko.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentTwo extends Fragment {
+
+    public FragmentTwo(){
+        super(R.layout.fragment_two);
+    }
+
+}
