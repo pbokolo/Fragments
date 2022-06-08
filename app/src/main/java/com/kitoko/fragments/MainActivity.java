7da6d6d3a61a69deac67824d9f7bbc9e0e65a67e
pbokolo <pbokolo1@gmail.com>
2022-06-08 20:56:02 +0300

    Ajout des fragments programmatiquement

--- app/src/main/java/com/kitoko/fragments/MainActivity.java
+++ app/src/main/java/com/kitoko/fragments/MainActivity.java
@@ -1,8 +1,11 @@
 package com.kitoko.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickMeBtn = findViewById(R.id.clickMeBtn);
+
+        clickMeBtn.setOnClickListener(v -> {
+
+            // 1-Recupere le fragment manager a partir de l'activite
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            // 2-Initialise l'operation de transaction(changement de fragment dans le fragment manager)
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            // 3-Precise l'endroit ou sera afficher le fragment ainsi que la classe du fragment a afficher
+            // 4-Effectue l'operation via la methode commit()
+            fragmentTransaction.add(R.id.fragmentContainer, FragmentOne.class, null).commit();
+
+           /* getSupportFragmentManager() // Recupere le fragment manager de l'activite
+                    .beginTransaction() // Lance une operation de transaction
+                    .add(R.id.fragmentContainer, FragmentOne.class, null) // Definit l'endroit a afficher le fragment et le fragment a afficher
+                    .commit(); // Effectue l'operation*/
+
+        });
+
     }
 }
