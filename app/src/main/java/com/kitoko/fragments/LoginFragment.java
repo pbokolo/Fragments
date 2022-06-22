cf5b9e6f42f0575b7e543039f043509194d36c06
pbokolo <pbokolo1@gmail.com>
2022-06-22 20:07:00 +0300

    Navigation vers le fragment d'inscription

--- app/src/main/java/com/kitoko/fragments/LoginFragment.java
+++ app/src/main/java/com/kitoko/fragments/LoginFragment.java
@@ -14,10 +14,12 @@ import android.widget.Button;
 
 public class LoginFragment extends Fragment {
 
+    private FragmentManager fragMan;
+    private FragmentTransaction fragTrans;
+
     public LoginFragment() {
 
     }
-
 
 
     @Override
@@ -34,25 +36,25 @@ public class LoginFragment extends Fragment {
         // C'est l'equivalent de setContentView() dans une activite
         View vue = inflater.inflate(R.layout.fragment_login, container, false);
 
-        Button conBtn = vue.findViewById(R.id.conBtn);
+        Button conBtn = vue.findViewById(R.id.conBtn),
+                inscrBtn = vue.findViewById(R.id.inscrBtn);
 
-        conBtn.setOnClickListener(v -> {
+        // 1-Recupere le fragment manager de l'activite hote
+        // 2-Initialiser une transaction(navigation ou alors changement de fragment a afficher) a partir du fragment manager
+         fragMan = getParentFragmentManager();
+         fragTrans = fragMan.beginTransaction();
 
-            // 1-Recupere le fragment manager de l'activite hote
-            // 2-Initialiser une transaction(navigation ou alors changement de fragment a afficher) a partir du fragment manager
-            // 3-Remplace l'ancien fragment par le nouveau
-            // 4-Ajoute le fragment dans le backstack(pour mettre en place une navigation fluide)
-            // 5-Commit(effectue la navigation)
-            FragmentManager fragMan = getParentFragmentManager();
-            FragmentTransaction fragTrans = fragMan.beginTransaction();
-            fragTrans.replace(R.id.fragmentContainer, MainFragment.class, null);
-            fragTrans.addToBackStack("nav");
-            fragTrans.commit();
-
-
-
-        });
-
+        conBtn.setOnClickListener(v -> replaceFragment(MainFragment.class));
+        inscrBtn.setOnClickListener(v -> replaceFragment(SignUpFragment.class));
         return vue;
+    }
+
+    void replaceFragment(Class frag){
+        // 3-Remplace l'ancien fragment par le nouveau
+        // 4-Ajoute le fragment dans le backstack(pour mettre en place une navigation fluide)
+        // 5-Commit(effectue la navigation)
+        fragTrans.replace(R.id.fragmentContainer, frag, null);
+        fragTrans.addToBackStack("nav");
+        fragTrans.commit();
     }
 }
